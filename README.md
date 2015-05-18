6e5aeffa487c820f60acc1e3e6d74363b4aedd95
Rob Jansen <jansen@cs.umn.edu>
2015-05-18 16:55:29 -0400

    finished analysis script to visualize results, added more doc to the readme

--- README.md
+++ README.md
@@ -1,3 +1,83 @@
 # OnionPerf
 
-A tool for monitoring the performance of onion services in Tor
+OnionPerf is a utility to track the performance of hidden services in Tor.
+
+OnionPerf uses multiple processes and threads to download random data
+through Tor while tracking the performance of those downloads. The data is
+served and fetched on localhost using two TGen (traffic generator)
+processes, but is tranferred through Tor using a temporary Tor Hidden
+Service process and a client process. Tor control information and TGen
+performance statistics are logged to disk and can be later analyzed (using
+onionperf-analyze.py) to vizualize changes in Tor performance over time.
+
+For more information, see https://github.com/robgjansen/onionperf.
+
+### Install Dependencies
+
+  + onionperf: python, python-stem
+  + tor: tor, libevent, openssl
+  + tgen: git, cmake, glib, igraph
+
+(You can build a custom Tor instead of using the package manager if you 
+want, and will be able to point OnionPerf at your custom Tor binary later.)
+
+Fedora/RedHat:
+
+```
+# for onionperf
+sudo yum install python python-stem tor git gcc cmake make glib2 glib2-devel igraph igraph-devel
+# for onionperf-analyze.py
+sudo yum install python numpy scipy python-matplotlib
+```
+
+Ubuntu/Debian:
+
+```
+# for onionperf
+sudo apt-get install python python-stem tor git gcc cmake make libglib2.0 libglib2.0-dev libigraph0 libigraph0-dev
+# for onionperf-analyze.py
+sudo apt-get install python python-matplotlib python-numpy python-scipy
+```
+
+### Build TGen Traffic Generator
+
+The traffic generator currently exists in the Shadow simulator repository,
+but we will build tgen as an external tool and skip building both the full 
+simulator and the tgen simulator plugin.
+
+```
+git clone git@github.com:shadow/shadow.git
+cd shadow/src/plugin/shadow-plugin-tgen
+mkdir build
+cd build
+cmake .. -DSKIP_SHADOW=ON -DCMAKE_MODULE_PATH=`pwd`/../../../../cmake/
+make
+```
+
+### Run OnionPerf
+
+You will need to give onionperf the path to your custom 'tor' and 'tgen'
+binary files if they do not exist in your PATH environment variable.
+
+```
+git clone git@github.com:robgjansen/onionperf.git
+cd onionperf
+./onionperf -h
+./onionperf --tor=/home/rob/tor/src/or/tor --tgen=/home/rob/shadow/src/plugin/shadow-plugin-tgen/build/tgen
+```
+
+This will store all local state, including log files, to the `.onionperf`
+directory. Let it run, and by default it will download files over Tor every
+60 minutes, and log the results.
+
+### Analyze Results
+
+You can run the client tgen log file through `onionperf-analyze.py` to
+visualize changes in performance over time.
+
+```
+python onionperf-analyze.py .onionperf/client/tgen/tgen.log
+```
+
+This will save a new PDF in the current directory.
+
--- onionperf-analyze.py
+++ onionperf-analyze.py
@@ -1,10 +1,48 @@
 #!/usr/bin/python
-import sys, os, argparse
+
+import matplotlib; matplotlib.use('Agg') # for systems without X11
+from matplotlib.backends.backend_pdf import PdfPages
+import sys, os, argparse, pylab, numpy, time
 from subprocess import Popen, PIPE
 
 DESCRIPTION="""
-A utility to help analyze onionperf output. Currently, this parses tgen transfer complete messages, and plots the results.
+A utility to help analyze onionperf output. Currently, this parses tgen transfer complete messages, and plots the results to a PDF file that is saved in the current directory.
 """
+
+pylab.rcParams.update({
+    'backend': 'PDF',
+    'font.size': 16,
+    'figure.max_num_figures' : 50,
+    'figure.figsize': (6,4.5),
+    'figure.dpi': 100.0,
+    'figure.subplot.left': 0.10,
+    'figure.subplot.right': 0.95,
+    'figure.subplot.bottom': 0.13,
+    'figure.subplot.top': 0.92,
+    'grid.color': '0.1',
+    'axes.grid' : True,
+    'axes.titlesize' : 'small',
+    'axes.labelsize' : 'small',
+    'axes.formatter.limits': (-4,4),
+    'xtick.labelsize' : 'small',
+    'ytick.labelsize' : 'small',
+    'lines.linewidth' : 2.0,
+    'lines.markeredgewidth' : 0.5,
+    'lines.markersize' : 10,
+    'legend.fontsize' : 'x-small',
+    'legend.fancybox' : False,
+    'legend.shadow' : False,
+    'legend.ncol' : 1.0,
+    'legend.borderaxespad' : 0.5,
+    'legend.numpoints' : 1,
+    'legend.handletextpad' : 0.5,
+    'legend.handlelength' : 1.6,
+    'legend.labelspacing' : .75,
+    'legend.markerscale' : 1.0,
+    'ps.useafm' : True,
+    'pdf.use14corefonts' : True,
+    'text.usetex' : True,
+})
 
 def main():
     parser = argparse.ArgumentParser(
@@ -29,38 +67,208 @@ def main():
     for line in args.datasource:
         try:
             parts = line.strip('\n').split(' ')
-            if len(parts) < 3 or parts[2].find('.') < 0: continue
-            # check for our log line types
-            if 'transfer-complete' in parts[4] or 'transfer-error' in parts[4]: tgen_parse(args, parts)
+            parse(args, parts)
         except: continue
     if 'xzproc' in args: args.xzproc.wait()
 
-def tgen_parse(args, parts):
-    ioparts = parts[11].split('=')
+    page = PdfPages("onionperf.results.{0}.pdf".format(time.strftime("%Y-%m-%d_%H:%M:%S")))
+    plot(args, page)
+    page.close()
+
+def parse(args, parts):
+    if len(parts) < 19: return
+    if 'transfer-complete' not in parts[5] and 'transfer-error' not in parts[5]: return
+
+    ioparts = parts[12].split('=')
     iodirection = ioparts[0]
     if 'read' not in iodirection: return
 
+    tstamp = float(parts[2])
     bytes = int(ioparts[1].split('/')[0])
 
-    if results not in args: args.results = {'firstbyte':{}, 'lastbyte':{}, 'errors':{}}
+    if 'results' not in args: args.results = {'firstbyte':{}, 'lastbyte':{}, 'errors':{}}
 
-    if 'transfer-complete' in parts[4]:
-        cmdtime = int(parts[13].split('=')[1])/1000.0
-        rsptime = int(parts[14].split('=')[1])/1000.0
-        fbtime = int(parts[15].split('=')[1])/1000.0
-        lbtime = int(parts[16].split('=')[1])/1000.0
-        chktime = int(parts[17].split('=')[1])/1000.0
+    if 'transfer-complete' in parts[5]:
+        cmdtime = int(parts[14].split('=')[1])/1000.0
+        rsptime = int(parts[15].split('=')[1])/1000.0
+        fbtime = int(parts[16].split('=')[1])/1000.0
+        lbtime = int(parts[17].split('=')[1])/1000.0
+        chktime = int(parts[18].split('=')[1])/1000.0
 
         if bytes not in args.results['firstbyte']: args.results['firstbyte'][bytes] = []
-        args.results['firstbyte'][bytes].append(fbtime-cmdtime)
+        args.results['firstbyte'][bytes].append((tstamp, fbtime-cmdtime))
 
         if bytes not in args.results['lastbyte']: args.results['lastbyte'][bytes] = []
-        args.results['lastbyte'][bytes].append(lbtime-cmdtime)
+        args.results['lastbyte'][bytes].append((tstamp, lbtime-cmdtime))
 
-    elif 'transfer-error' in parts[4]:
-        code = parts[8].strip('()').split('-')[7].split('=')[1]
+    elif 'transfer-error' in parts[5]:
+        code = parts[9].strip('()').split('-')[7].split('=')[1]
 
         if code not in args.results['errors']: args.results['errors'][code] = []
-        args.results['errors'][code].append(bytes)
+        args.results['errors'][code].append((tstamp, bytes))
+
+def plot(args, page):
+    colors = {51200:'r', 1048576:'b', 5242880:'g'}
+    hours = {}
+
+    # first byte, time series
+    d = {}
+    for bytes in args.results['firstbyte']:
+        for (ts, t) in args.results['firstbyte'][bytes]: d[ts] = t
+
+    pylab.figure()
+
+    keys = sorted(d.keys())
+    num_hours = (keys[-1]-keys[0])/3600.0
+    x = [(k - keys[0])/3600.0 for k in keys]
+    y = [d[k] for k in keys]
+
+    for i in x:
+        if int(i) not in hours: hours[int(i)] = True
+
+    y_ma = movingaverage(y, int(num_hours/10.0))
+    pylab.scatter(x, y, c='k', s=3.0, label='raw')
+    pylab.plot(x, y_ma, 'r-', label='smoothed')
+
+    pylab.xlabel("Time Span (h)")
+    pylab.xlim(xmin=0, xmax=num_hours)
+    pylab.ylabel("Download Time (s)")
+    pylab.ylim(ymin=0, ymax=10.0)
+    pylab.title("OnionPerf: time to download first byte")
+    pylab.legend(loc="best")
+    page.savefig()
+    pylab.close()
+
+    # first byte, cdf
+    d = {}
+    for bytes in args.results['firstbyte']:
+        if bytes not in [51200, 1048576, 5242880]: continue
+        if bytes not in d: d[bytes] = {}
+        for (ts, t) in args.results['firstbyte'][bytes]: d[bytes][ts] = t
+
+    pylab.figure()
+
+    maxx = 0
+    for bytes in d:
+        keys = sorted(d[bytes].keys())
+        num_hours = (keys[-1]-keys[0])/3600.0
+        vals = [d[bytes][k] for k in keys]
+        x, y = getcdf(vals)
+        pylab.plot(x, y, '-', c=colors[bytes], label="{0} KiB".format(int(bytes/1024.0)))
+        if x[-1] > maxx: maxx = x[-1]
+
+    pylab.xlabel("Download Time (s)")
+    pylab.xlim(xmin=0, xmax=maxx)
+    pylab.ylabel("Cumulative Fraction")
+    pylab.ylim(ymin=0, ymax=1.0)
+    pylab.title("OnionPerf: time to download first byte")
+    pylab.legend(loc="lower right")
+    page.savefig()
+    pylab.close()
+
+    # last byte, time series
+    d = {}
+    for bytes in args.results['lastbyte']:
+        if bytes not in [51200, 1048576, 5242880]: continue
+        if bytes not in d: d[bytes] = {}
+        for (ts, t) in args.results['lastbyte'][bytes]: d[bytes][ts] = t
+
+    pylab.figure()
+
+    for bytes in d:
+        keys = sorted(d[bytes].keys())
+        num_hours = (keys[-1]-keys[0])/3600.0
+        x = [(k - keys[0])/3600.0 for k in keys]
+        y = [d[bytes][k] for k in keys]
+
+        y_ma = movingaverage(y, int(num_hours/10.0))
+        pylab.scatter(x, y, c=colors[bytes], edgecolor=colors[bytes], s=3.0)
+        pylab.plot(x, y_ma, '-', c=colors[bytes], label="{0} KiB".format(int(bytes/1024.0)))
+
+    pylab.xlabel("Time Span (h)")
+    pylab.xlim(xmin=0, xmax=num_hours)
+    pylab.ylabel("Download Time (s)")
+    pylab.ylim(ymin=0, ymax=60.0)
+    pylab.title("OnionPerf: time to download last byte")
+    pylab.legend(loc="best")
+    page.savefig()
+    pylab.close()
+
+    # last byte, cdf
+    pylab.figure()
+
+    maxx = 0
+    for bytes in d:
+        keys = sorted(d[bytes].keys())
+        num_hours = (keys[-1]-keys[0])/3600.0
+        vals = [d[bytes][k] for k in keys]
+        x, y = getcdf(vals)
+        pylab.plot(x, y, '-', c=colors[bytes], label="{0} KiB".format(int(bytes/1024.0)))
+        if x[-1] > maxx: maxx = x[-1]
+
+    pylab.xlabel("Download Time (s)")
+    pylab.xlim(xmin=0, xmax=maxx)
+    pylab.ylabel("Cumulative Fraction")
+    pylab.ylim(ymin=0, ymax=1.0)
+    pylab.title("OnionPerf: time to download last byte")
+    pylab.legend(loc="lower right")
+    page.savefig()
+    pylab.close()
+
+    # errors, time series
+    d = {51200:{}, 1048576:{}, 5242880:{}}
+    for h in hours:
+        for b in d: d[b][int(h)] = 0
+
+    mints = None
+    for code in args.results['errors']:
+        for (ts, bytes) in args.results['errors'][code]:
+            if bytes not in d: continue
+            if mints == None: mints = ts
+            h = int((ts-mints)/3600.0)
+            d[bytes][h] += 1
+
+    pylab.figure()
+
+    maxy = 1
+    for bytes in d:
+        x = sorted(d[bytes].keys())
+        y = [d[bytes][k] for k in x]
+        if max(y) > maxy: maxy = max(y)
+        pylab.scatter(x, y, c=colors[bytes], edgecolor=colors[bytes], s=10.0, label="{0} KiB".format(int(bytes/1024.0)))
+
+    pylab.xlabel("Time Span (h)")
+    pylab.xlim(xmin=0, xmax=num_hours)
+    pylab.ylabel("Number of Errors Per Hour")
+    pylab.ylim(ymin=0, ymax=maxy)
+    pylab.title("OnionPerf: number of errors")
+    pylab.legend(loc="best")
+    page.savefig()
+    pylab.close()
+
+# helper - compute the window_size moving average over the data in interval
+def movingaverage(interval, window_size):
+    window = numpy.ones(int(window_size))/float(window_size)
+    return numpy.convolve(interval, window, 'same')
+
+## helper - cumulative fraction for y axis
+def cf(d): return pylab.arange(1.0,float(len(d))+1.0)/float(len(d))
+
+## helper - return step-based CDF x and y values
+## only show to the 99th percentile by default
+def getcdf(data, shownpercentile=0.99, maxpoints=10000.0):
+    data.sort()
+    frac = cf(data)
+    k = len(data)/maxpoints
+    x, y, lasty = [], [], 0.0
+    for i in xrange(int(round(len(data)*shownpercentile))):
+        if i % k > 1.0: continue
+        assert not numpy.isnan(data[i])
+        x.append(data[i])
+        y.append(lasty)
+        x.append(data[i])
+        y.append(frac[i])
+        lasty = frac[i]
+    return x, y
 
 if __name__ == '__main__': sys.exit(main())
