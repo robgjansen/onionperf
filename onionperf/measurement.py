693f0f6ca2f66b8ff468aa3d47efc2272a9a4145
Rob Jansen <jansen@cs.umn.edu>
2015-10-19 09:29:49 -0400

    hidden onion service mode seems to work

--- onionperf/measurement.py
+++ onionperf/measurement.py
@@ -4,21 +4,14 @@ Created on Oct 1, 2015
 @author: rob
 '''
 
-import sys, os, shutil, subprocess, multiprocessing, argparse, logging, time
-from signal import signal, SIGINT, SIG_IGN, SIGTERM
-from itertools import cycle
-from functools import partial
+import os, subprocess, multiprocessing, logging, time, socket
+from signal import signal, SIGINT, SIG_IGN
 
-import stem
-from stem import process, control
-from stem.control import EventType, Controller
-from stem.util import str_tools
+import stem.process
 from abc import abstractmethod, ABCMeta
 
 import monitor, model
 import re
-
-logging.getLogger("stem").setLevel(logging.WARN)
 
 class Process(object):
 
@@ -43,7 +36,7 @@ class TGenProcess(Process):
         self.datadir_path = datadir_path
         self.tgen_model = tgen_model
         self.tgen_proc = None
-        self.tgen_p = None
+        self.tgen_subp = None
         self.tgen_log_path = None
 
     def start(self, done_event):
@@ -60,32 +53,32 @@ class TGenProcess(Process):
         self.tgen_model.dump_to_file(conffile)
 
         self.tgen_log_path = "{0}/onionperf.tgen.log".format(self.datadir_path)
+        logging.info("logging TGen process output to {0}".format(self.tgen_log_path))
         with open(self.tgen_log_path, 'a') as logf:
-            self.tgen_p = None
+            self.tgen_subp = None
             while not done_event.is_set():
-                self.tgen_p = subprocess.Popen([self.bin_path, conffile], stdout=logf, stderr=logf)
+                self.tgen_subp = subprocess.Popen([self.bin_path, conffile], stdout=logf, stderr=logf)
                 while not done_event.wait(5):  # while the timeout triggers
-                    if self.tgen_p.poll() is not None:  # process has terminated
-                        self.tgen_p.wait()  # collect child
-                        self.tgen_p = None  # clear
+                    if self.tgen_subp.poll() is not None:  # process has terminated
+                        self.tgen_subp.wait()  # collect child
+                        self.tgen_subp = None  # clear
                         break  # break out inner loop and restart process
-            # if interupted and proc is still running, kill it
-            if self.tgen_p is not None and self.tgen_p.poll() is None:
-                self.tgen_p.terminate()
-                self.tgen_p.wait()
 
     def is_alive(self):
-        if self.tgen_proc is not None and self.tgen_proc.is_alive():
+        if self.tgen_subp is not None and self.tgen_subp.poll() is None and self.tgen_proc is not None and self.tgen_proc.is_alive():
             return True
         else:
             return False
 
     def stop(self):
-        if self.tgen_p is not None and self.tgen_proc is not None:
-            self.tgen_p.terminate()
+        if self.tgen_subp is not None and self.tgen_subp.poll() is None:
+            self.tgen_subp.terminate()
+            self.tgen_subp.wait()
+            self.tgen_subp = None
+        if self.tgen_proc is not None:
+            self.tgen_proc.terminate()
             self.tgen_proc.join()
             self.tgen_proc = None
-            self.tgen_p = None
 
     def get_download_count(self):
         count = 0
@@ -101,9 +94,9 @@ class TorProcess(Process):
     def __init__(self, bin_path, datadir_path):
         self.bin_path = bin_path
         self.datadir_path = datadir_path
-        self.tor_stem_p = None
+        self.tor_subp = None
         self.tor_monitor_proc = None
-        self.self.hs_service_id = None
+        self.hs_service_id = None
 
     def start(self, done_event, control_port=9050, socks_port=9000, hs_port_mapping=None):
         if not os.path.exists(self.datadir_path): os.makedirs(self.datadir_path)
@@ -128,34 +121,41 @@ class TorProcess(Process):
             ],
         }
 
-        self.tor_stem_p = stem.process.launch_tor_with_config(config, tor_cmd=self.bin_path, completion_percent=100, init_msg_handler=None, timeout=None, take_ownership=True)
+        self.tor_subp = stem.process.launch_tor_with_config(config, tor_cmd=self.bin_path, completion_percent=100, init_msg_handler=None, timeout=None, take_ownership=False)
+        self.control_port = control_port
 
-        tor_monitor = monitor.TorMonitor(control_port, "{0}/onionperf.tor.log".format(self.datadir_path))
+        self.tor_log_path = "{0}/onionperf.tor.log".format(self.datadir_path)
+        logging.info("logging Tor events from port {0} to {1}".format(control_port, self.tor_log_path))
+        tor_monitor = monitor.TorMonitor(control_port, self.tor_log_path)
         self.tor_monitor_proc = multiprocessing.Process(target=monitor.TorMonitor.run, args=(tor_monitor,))
         self.tor_monitor_proc.start()
 
         if hs_port_mapping is not None:
             # TODO how do we check to make sure tor supports ephemeral HS (version >= 0.2.7.1-alpha)?
-            with stem.control.Controller.from_port(port=self.tor_ctl_port) as torctl:
+            with stem.control.Controller.from_port(port=self.control_port) as torctl:
                 torctl.authenticate()
                 response = torctl.create_ephemeral_hidden_service(hs_port_mapping, detached=True, await_publication=True)
                 self.hs_service_id = response.service_id
 
     def stop(self):
         if self.hs_service_id is not None:
-            with stem.control.Controller.from_port(port=self.tor_ctl_port) as torctl:
+            with stem.control.Controller.from_port(port=self.control_port) as torctl:
                 torctl.authenticate()
                 torctl.remove_ephemeral_hidden_service(self.hs_service_id)
+        if self.tor_subp is not None and self.tor_subp.poll() is None:
+            self.tor_subp.terminate()
+            self.tor_subp.wait()
+            self.tor_subp = None
         if self.tor_monitor_proc is not None:
             self.tor_monitor_proc.terminate()
             self.tor_monitor_proc.join()
             self.tor_monitor_proc = None
 
     def is_alive(self):
-        alive = False
-        if (self.tor_stem_p is not None and self.tor_stem_p.poll() is not None) and (self.tor_monitor_proc is not None and self.tor_monitor_proc.is_alive()):
-            alive = True
-        return alive
+        if self.tor_subp is not None and self.tor_subp.poll() is None and self.tor_monitor_proc is not None and self.tor_monitor_proc.is_alive():
+            return True
+        else:
+            return False
 
 class Measurement(object):
 
@@ -163,13 +163,17 @@ class Measurement(object):
         self.tor_bin_path = tor_bin_path
         self.tgen_bin_path = tgen_bin_path
         self.datadir_path = datadir_path
-        self.tgen_client = None
-        self.tgen_server = None
         self.done_event = None
+        self.tgen_server = None
+        self.tor_server = None
+        self.tor_client = None
+        self.tgen_client = None
 
-    def run(self):
+    def run(self, do_onion=True, do_inet=False, do_local=False):
         # if ctrl-c is pressed, shutdown child processes properly
         try:
+            logging.info("Bootstrapping started...")
+            logging.info("Log files for the client and server processes will be placed in {0}".format(self.datadir_path))
             self.done_event = multiprocessing.Event()
 
             logging.info("Starting tgen server process...")
@@ -177,23 +181,35 @@ class Measurement(object):
             self.tgen_server = TGenProcess(self.tgen_bin_path, "{0}/tgen-server".format(self.datadir_path), tgen_server_model)
             self.tgen_server.start(self.done_event)
 
-            # logging.info("Starting tor server process...")
+            logging.info("Starting tor server process...")
+            self.tor_server = TorProcess(self.tor_bin_path, "{0}/tor-server".format(self.datadir_path))
+            self.tor_server.start(self.done_event, control_port=9050, socks_port=0, hs_port_mapping={58888:58888})
 
+            logging.info("Starting tor client process...")
+            self.tor_client = TorProcess(self.tor_bin_path, "{0}/tor-client".format(self.datadir_path))
+            self.tor_client.start(self.done_event, control_port=9051, socks_port=9001, hs_port_mapping=None)
 
-            # logging.info("Starting tor client process...")
+            server_urls = []
+            if do_onion:
+                server_urls.append("{0}.onion:58888".format(self.tor_server.hs_service_id))
+            if do_inet:
+                server_urls.append("{0}:58888".format(self.__get_ip_address()))
+            if do_local:
+                server_urls.append("127.0.0.1:58888")
 
+            if len(server_urls) > 0:
+                logging.info("Starting tgen client process...")
+                tgen_client_model = model.TorperfModel(tgen_port="58889", tgen_servers=server_urls, socksproxy="127.0.0.1:9001")
+                self.tgen_client = TGenProcess(self.tgen_bin_path, "{0}/tgen-client".format(self.datadir_path), tgen_client_model)
+                self.tgen_client.start(self.done_event)
 
-            logging.info("Starting tgen client process...")
-            tgen_client_model = model.TorperfModel(tgen_port="58889", tgen_servers=["127.0.0.1:58888"])
-            self.tgen_client = TGenProcess(self.tgen_bin_path, "{0}/tgen-client".format(self.datadir_path), tgen_client_model)
-            self.tgen_client.start(self.done_event)
-
-            logging.info("Log files for the client and server processes are located in {0}".format(self.datadir_path))
-
+            logging.info("Bootstrapping finished, entering heartbeat loop")
             while True:
                 logging.info("Heartbeat: {0} downloads have completed successfully".format(self.tgen_client.get_download_count()))
-                if self.__is_alive(): logging.info("All helper process seem to be alive :)")
-                else: logging.warning("Some parallel components have died :(")
+                if self.__is_alive():
+                    logging.info("All helper process seem to be alive :)")
+                else:
+                    logging.warning("Some parallel components have died :(")
                 logging.info("Main process will now sleep for 1 hour (helper processes run on their own schedule)")
                 logging.info("press CTRL-C for graceful shutdown...")
                 time.sleep(3600)
@@ -208,8 +224,14 @@ class Measurement(object):
             if self.done_event is not None:
                 self.done_event.set()
 
-            if self.tgen_server is not None: self.tgen_server.stop()
-            if self.tgen_client is not None: self.tgen_client.stop()
+            if self.tgen_client is not None:
+                self.tgen_client.stop()
+            if self.tor_client is not None:
+                self.tor_client.stop()
+            if self.tor_server is not None:
+                self.tor_server.stop()
+            if self.tgen_server is not None:
+                self.tgen_server.stop()
 
             time.sleep(1)
             logging.disable(logging.NOTSET)
@@ -218,12 +240,23 @@ class Measurement(object):
             logging.info("Child process cleanup complete!")
             logging.info("Exiting")
 
+    def __get_ip_address(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        return s.getsockname()[0]
+
     def __is_alive(self):
         alive = True
         if self.tgen_server is not None and not self.tgen_server.is_alive():
-            logging.info("server tgen process is dead")
+            logging.warning("server tgen process is dead")
+            alive = False
+        if self.tor_server is not None and not self.tor_server.is_alive():
+            logging.warning("server tor process is dead")
+            alive = False
+        if self.tor_client is not None and not self.tor_client.is_alive():
+            logging.warning("client tor process is dead")
             alive = False
         if self.tgen_client is not None and not self.tgen_client.is_alive():
-            logging.info("client tgen process is dead")
+            logging.warning("client tgen process is dead")
             alive = False
         return alive
--- onionperf/monitor.py
+++ onionperf/monitor.py
@@ -4,7 +4,7 @@ Created on Oct 10, 2015
 @author: rob
 '''
 
-import sys, os, time, stem
+import time, stem
 from functools import partial
 from stem.control import EventType, Controller
 
@@ -19,11 +19,8 @@ class TorMonitor(object):
 
     def run(self):
         sink = util.DataSink(self.filepath)
+        sink.open()
         logfile = sink.get()
-
-        startup_msg = "started tor-ctl-logger on port {0}, logging events to {1}\n".format(self.tor_ctl_port, self.filepath)
-        self.__log(logfile, startup_msg)
-        self.__log(sys.stderr, startup_msg)
 
         with stem.control.Controller.from_port(port=self.tor_ctl_port) as torctl:
             torctl.authenticate()
@@ -32,27 +29,31 @@ class TorMonitor(object):
             # some events are only supported in newer versions of tor, so ignore errors from older tors
             event_handler = partial(TorMonitor.__handle_tor_event, self, logfile,)
             for e in self.events:
-                try:
-                    if e in EventType:
+                if e in EventType:
+                    # try to add all events that this stem supports
+                    # silently ignore those that our Tor does not support
+                    try:
                         torctl.add_event_listener(event_handler, EventType[e])
-                except:
-                    pass
+                    except:
+                        pass
 
             # let stem run its threads and log all of the events, until user interrupts
             try:
                 while True:
-                    if self.filepath != '-' and os.path.exists(self.filepath):
-                        with open(self.filepath, 'rb') as sizef:
-                            msg = "heartbeat: logged {0} bytes to {1}, press CTRL-C to quit\n".format(os.fstat(sizef.fileno()).st_size, self.filepath)
-                            self.__log(sys.stderr, msg)
+                    # if self.filepath != '-' and os.path.exists(self.filepath):
+                    #    with open(self.filepath, 'rb') as sizef:
+                    #        msg = "tor-ctl-logger[port={0}] logged {1} bytes to {2}, press CTRL-C to quit".format(self.tor_ctl_port, os.fstat(sizef.fileno()).st_size, self.filepath)
+                    #        logging.info(msg)
                     time.sleep(60)
             except KeyboardInterrupt:
                 pass  # the user hit ctrl+c
+
+        sink.close()
 
     def __handle_tor_event(self, logfile, event):
         self.__log(logfile, event.raw_content())
 
     def __log(self, logfile, msg):
         s = time.time()
-        t = time.localtime(s)
+        # t = time.localtime(s)
         print >> logfile, "{0} {1} {2}".format(time.strftime("%Y-%m-%d %H:%M:%S"), s, msg),
