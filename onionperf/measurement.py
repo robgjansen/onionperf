41f17d4ba7a614a277736548016d24d7de5a22a1
Rob Jansen <jansen@cs.umn.edu>
2015-10-28 23:00:46 -0400

    replace stem's tor launch, small tweaks

--- onionperf/measurement.py
+++ onionperf/measurement.py
@@ -4,7 +4,8 @@ Created on Oct 1, 2015
 @author: rob
 '''
 
-import os, subprocess, threading, Queue, logging, time, socket, re, stem
+import os, subprocess, threading, Queue, logging, time, socket, re
+import stem, stem.process, stem.version, stem.util.str_tools
 from lxml import etree
 
 import monitor, model, util
@@ -16,7 +17,7 @@ def watchdog_task(subp, writable, done_ev):
     # get another helper to block on the subprocess stdout
     q = Queue.Queue()
     t = threading.Thread(target=getlines_task, args=(subp.stdout, q))
-    #t.setDaemon(True)
+    # t.setDaemon(True)
     t.start()
 
     while subp.poll() is None and done_ev.is_set() is False:
@@ -35,7 +36,7 @@ def watchdog_task(subp, writable, done_ev):
 
     # flush any remaining lines
     subp.stdout.close()
-    t.join()
+    # t.join()
     while not q.empty(): writable.write(q.get_nowait())
     writable.close()
 
@@ -48,17 +49,23 @@ class Measurement(object):
         self.datadir_path = datadir_path
         self.threads = None
         self.done_event = None
+        self.hs_service_id = None
 
-    def run(self, do_onion=True, do_inet=False, do_local=False):
+    def run(self, do_onion=True, do_inet=True):
         self.threads = []
         self.done_event = threading.Event()
 
         # if ctrl-c is pressed, shutdown child processes properly
         try:
+            # make sure tor supports ephemeral HS (version >= 0.2.7.1-alpha)?
+            if do_onion and stem.version.get_system_tor_version(self.tor_bin_path) < stem.version.Requirement.ADD_ONION:
+                logging.warning("OnionPerf in onion mode requires Tor version >= 0.2.7.1-alpha, aborting")
+                return
+
             logging.info("Bootstrapping started...")
             logging.info("Log files for the client and server processes will be placed in {0}".format(self.datadir_path))
 
-            if do_local or do_onion or do_inet:
+            if do_onion or do_inet:
                 self.__start_tgen_server()
 
             if do_onion:
@@ -68,11 +75,10 @@ class Measurement(object):
                 self.__start_tor_client()
 
             server_urls = []
-            if do_onion: server_urls.append("{0}.onion:58888".format(self.tor_server.hs_service_id))
+            if do_onion and self.hs_service_id is not None: server_urls.append("{0}.onion:58888".format(self.hs_service_id))
             if do_inet: server_urls.append("{0}:58888".format(self.__get_ip_address()))
-            if do_local: server_urls.append("127.0.0.1:58888")
 
-            if do_local or do_onion or do_inet:
+            if do_onion or do_inet:
                 assert len(server_urls) > 0
                 tgen_client_logpath = self.__start_tgen_client(server_urls)
                 self.__start_twistd()
@@ -99,6 +105,11 @@ class Measurement(object):
         finally:
             logging.info("Cleaning up child processes now...")
 
+            if self.hs_service_id is not None:
+                with stem.control.Controller.from_port(port=self.hs_control_port) as torctl:
+                    torctl.authenticate()
+                    torctl.remove_ephemeral_hidden_service(self.hs_service_id)
+
             logging.disable(logging.INFO)
             self.done_event.set()
             for t in self.threads: t.join()
@@ -116,7 +127,7 @@ class Measurement(object):
         return self.__start_tgen("server", 58888)
 
     def __start_tgen(self, name, tgen_port, socks_port=None, server_urls=None):
-        logging.info("Starting tgen {0} process...".format(name))
+        logging.info("Starting TGen {0} process...".format(name))
 
         tgen_datadir = "{0}/tgen-{1}".format(self.datadir_path, name)
         if not os.path.exists(tgen_datadir): os.makedirs(tgen_datadir)
@@ -141,7 +152,7 @@ class Measurement(object):
         return tgen_logpath
 
     def __start_twistd(self):
-        logging.info("Starting Twisted server process...")
+        logging.info("Starting Twistd server process...")
 
         twisted_datadir = "{0}/twisted-data".format(self.datadir_path)
         if not os.path.exists(twisted_datadir): os.makedirs(twisted_datadir)
@@ -170,53 +181,55 @@ class Measurement(object):
         return self.__start_tor("server", 59050, 59000, {58888: 58888})
 
     def __start_tor(self, name, control_port, socks_port, hs_port_mapping=None):
-        logging.info("Starting tor {0} process...".format(name))
+        logging.info("Starting Tor {0} process...".format(name))
 
         tor_datadir = "{0}/tor-{1}".format(self.datadir_path, name)
         if not os.path.exists(tor_datadir): os.makedirs(tor_datadir)
 
-        tor_config = {
-            'ORPort': '0',
-            'DirPort': '0',
-            'ControlPort': str(control_port),
-            'SocksPort': str(socks_port),
-            'SocksListenAddress': '127.0.0.1',
-            'ClientOnly': '1',
-            'WarnUnsafeSocks': '0',
-            'SafeLogging': '0',
-            'MaxCircuitDirtiness': '10 seconds',
-            'UseEntryGuards' : '0',
-            'DataDirectory': tor_datadir,
-            'Log': [
-                'INFO stdout',
-                # 'INFO file .onionperf/client/tor/tor.log'.format(self.c_tor_datadir),
-            ],
-        }
+        tor_config_template = "ORPort 0\nDirPort 0\nControlPort {0}\nSocksPort {1}\nSocksListenAddress 127.0.0.1\nClientOnly 1\n\
+WarnUnsafeSocks 0\nSafeLogging 0\nMaxCircuitDirtiness 10 seconds\nUseEntryGuards 0\nDataDirectory {2}\nLog INFO stdout\n"
+        tor_config = tor_config_template.format(control_port, socks_port, tor_datadir)
 
         tor_logpath = "{0}/onionperf.tor.log".format(tor_datadir)
         tor_writable = util.FileWritable(tor_logpath)
-        logging.info("logging Tor {0} process output to {1}".format(name, tor_logpath))
+        logging.info("Logging Tor {0} process output to {1}".format(name, tor_logpath))
 
-        tor_subp = stem.process.launch_tor_with_config(tor_config, tor_cmd=self.tor_bin_path, completion_percent=100, init_msg_handler=None, timeout=None, take_ownership=False)
+        # tor_subp = stem.process.launch_tor_with_config(tor_config, tor_cmd=self.tor_bin_path, completion_percent=100, init_msg_handler=None, timeout=None, take_ownership=False)
+        tor_cmd = "{0} -f -".format(self.tor_bin_path)
+        tor_subp = subprocess.Popen(tor_cmd.split(), cwd=tor_datadir, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.PIPE)
+        tor_subp.stdin.write(stem.util.str_tools._to_bytes(tor_config))
+        tor_subp.stdin.close()
+
+        # wait for Tor to bootstrap
+        boot_re = re.compile('Bootstrapped 100')
+        for line in iter(tor_subp.stdout.readline, b''):
+            tor_writable.write(line)
+            if boot_re.search(line): break
+
         tor_watchdog = threading.Thread(target=watchdog_task, name="tor_{0}_watchdog".format(name), args=(tor_subp, tor_writable, self.done_event))
         tor_watchdog.start()
         self.threads.append(tor_watchdog)
 
         torctl_logpath = "{0}/onionperf.torctl.log".format(tor_datadir)
         torctl_writable = util.FileWritable(torctl_logpath)
-        logging.info("logging Tor {0} control port monitor output to {1}".format(name, torctl_logpath))
+        logging.info("Logging Tor {0} control port monitor output to {1}".format(name, torctl_logpath))
 
-        torctl_monitor = monitor.TorMonitor(control_port, torctl_writable)
+        time.sleep(5)
+
+        torctl_events = [e for e in monitor.get_supported_torctl_events() if e not in ['DEBUG', 'INFO', 'NOTICE', 'WARN', 'ERR']]
+        torctl_monitor = monitor.TorMonitor(control_port, torctl_writable, events=torctl_events)
         torctl_helper = threading.Thread(target=monitor.TorMonitor.run, name="torctl_{0}_helper".format(name), args=(torctl_monitor,))
         torctl_helper.start()
         self.threads.append(torctl_helper)
 
         if hs_port_mapping is not None:
-            # TODO how do we check to make sure tor supports ephemeral HS (version >= 0.2.7.1-alpha)?
+            logging.info("Creating ephemeral hidden service...")
             with stem.control.Controller.from_port(port=control_port) as torctl:
                 torctl.authenticate()
                 response = torctl.create_ephemeral_hidden_service(hs_port_mapping, detached=True, await_publication=True)
                 self.hs_service_id = response.service_id
+                self.hs_control_port = control_port
+                logging.info("Ephemeral hidden service is available at {0}.onion".format(response.service_id))
 
         return torctl_logpath
 
