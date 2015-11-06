65121506851ec689773af324d7f4f611740957f5
Rob Jansen <jansen@cs.umn.edu>
2015-11-06 17:52:14 -0500

    padd tor monitor output

--- onionperf/monitor.py
+++ onionperf/monitor.py
@@ -53,7 +53,7 @@ class TorMonitor(object):
     def __log(self, writable, msg):
         s = time.time()
         # t = time.localtime(s)
-        writable.write("{0} {1} {2}".format(time.strftime("%Y-%m-%d %H:%M:%S"), s, msg))
+        writable.write("{0} {1:.02f} {2}".format(time.strftime("%Y-%m-%d %H:%M:%S"), s, msg))
 
 def tor_monitor_run(tor_ctl_port, writable, events, done_ev):
     torctl_monitor = TorMonitor(tor_ctl_port, writable, events)
